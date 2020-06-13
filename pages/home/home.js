a6517190cae26be13217b9dfa2fdcfd2b850b600
Nevermind1 <l>
2020-06-13 21:32:26 +0800

    提交

---
+++ pages/home/home.js
@@ -0,0 +1,25 @@
+Page({
+  data:{
+    name:'王德发',
+    student:[
+      {id:110,name:'张三',age:19},
+      {id:111,name:'李四',age:29},
+      {id:112,name:'王二',age:39},
+      {id:113,name:'麻子',age:49}
+    ],
+    count:0
+  },
+
+    add(){
+      this.setData({
+        count:this.data.count+1
+      })
+      // return this.data.count++
+    },
+    jian(){
+      this.setData({
+        count:this.data.count -1
+      })
+    }
+  
+})
